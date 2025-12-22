a04d1d9eaf1451d51c554f87560bf1e2011a30f7
kunnoh <alvindereba@gmail.com>
2025-12-22 15:03:14 +0300

    Socket listening

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use log::{debug, error, info};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger:: init();
+
+    info!("Server starting!");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    info!("Server listening on: http://{}", listener.local_addr()?);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                debug!("Accepted new connection:: {}", stream.peer_addr()?);
+            }
+            Err(e) => error!("Err: \n{e}")
+        }
+    }
+    Ok(())
 }
