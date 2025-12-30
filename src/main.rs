c917cba9902073f221a706365150f1744ed516c8
kunnoh <alvindereba@gmail.com>
2025-12-30 20:43:06 +0300

    responding with path as body Add setting headers

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,46 @@ fn handle_connection(stream: &mut TcpStream) -> Result<()> {
 
     match &path_vec[..] {
         ["GET", path, "HTTP/1.1"] => {
+            info!("GET {path}");
             if *path == "/" {
-                info!("GET - {path}");
                 stream.write(SUCCESS_RESPONSE)?;
+                stream.flush()
+            } else if path.starts_with("/echo/") {
+                let echo_path: Option<(&str, &str)> = path.split_once("/echo");
+                match echo_path {
+                    Some((_, path)) => {
+                        let response: String = format!(
+                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", 
+                            path.len(), 
+                            path
+                        );
+                        
+                        stream.write(response.as_bytes());
+                        stream.flush()
+                    },
+                    _ => {
+                        error!("Invalid path: {path}");
+                        stream.write(ERROR_RESPONSE);
+                        stream.flush()
+                    }
+                }
             } else {
-                warn!("GET - {path}");
-                stream.write(ERROR_RESPONSE)?;
+                warn!("GET {path}");
+                let response: String = format!(
+                    "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", 
+                    path.len(), 
+                    path
+                );
+                stream.write(response.as_bytes())?;
+                stream.flush()
             }
         }
         _ => {
             error!("Invalid request: {}", request_line.trim());
             stream.write(ERROR_RESPONSE)?;
+            stream.flush()
         }
     }
-
-    stream.flush()
 }
 
 fn main() -> Result<()> {
