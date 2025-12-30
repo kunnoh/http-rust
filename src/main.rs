32975873aba6ddee30209745002094e83b10dc57
kunnoh <alvindereba@gmail.com>
2025-12-30 17:53:35 +0300

    respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use log::{debug, error, info, warn};
+use std::io::{BufRead, BufReader, Result, Write};
 use std::net::{TcpListener, TcpStream};
-use std::io::{BufRead, BufReader, Write, Result};
-use log::{debug, error, info};
 
 const SUCCESS_RESPONSE: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
 const ERROR_RESPONSE: &[u8] = b"HTTP/1.1 404 Not Found\r\n\r\n";
@@ -12,23 +12,22 @@ fn handle_connection(stream: &mut TcpStream) -> Result<()> {
     let mut request_line: String = String::new();
     request_buffer.read_line(&mut request_line)?;
 
-    // Split by space. Second field is path
-    // Example: "GET /index.html HTTP/1.1"
     let path_vec: Vec<&str> = request_line.split_whitespace().collect();
+    // println!("path: \n\t{:?}", path_vec);
 
     match &path_vec[..] {
         ["GET", path, "HTTP/1.1"] => {
             if *path == "/" {
-                debug!("/ path requested");
-                stream.write_all(SUCCESS_RESPONSE)?;
+                info!("GET - {path}");
+                stream.write(SUCCESS_RESPONSE)?;
             } else {
-                debug!("Unknown path {} requested", path);
-                stream.write_all(ERROR_RESPONSE)?;
+                warn!("GET - {path}");
+                stream.write(ERROR_RESPONSE)?;
             }
         }
         _ => {
             error!("Invalid request: {}", request_line.trim());
-            stream.write_all(ERROR_RESPONSE)?;
+            stream.write(ERROR_RESPONSE)?;
         }
     }
 
@@ -36,20 +35,19 @@ fn handle_connection(stream: &mut TcpStream) -> Result<()> {
 }
 
 fn main() -> Result<()> {
-    env_logger:: init();
+    env_logger::init();
 
-    info!("Server starting!");
     let listener = TcpListener::bind("127.0.0.1:4221")?;
     info!("Server started at: http://{}", listener.local_addr()?);
-    
+
     for tcp_stream in listener.incoming() {
         match tcp_stream {
             Ok(mut stream) => {
                 if let Err(e) = handle_connection(&mut stream) {
-                    error!("Error handle_connection: {e}");
-                } 
+                    error!("Error handle_connection:\n\t {e}");
+                }
             }
-            Err(e) => error!("Connection failed:: {e}")
+            Err(e) => error!("Connection failed:\n\t {e}"),
         }
     }
 
